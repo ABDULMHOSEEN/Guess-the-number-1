43aa79b6cb6959d537e7800501f21d69b31c9ad5
ABDULMOHSEEN <aam20122003@gmail.com>
2021-07-24 01:50:44 +0300

    Fix some broplem - change the range - add the hint

--- Guess The Number.py
+++ Guess The Number.py
@@ -1,28 +1,48 @@
 # Guess the number game
+# import
+from random import randint  # imports the random integer function from the random module
+from math import *
 
-from random import randint # imports the random integer function from the random module
+# Take value
+unknownNumber = randint(1, 100)  # sets the range of the unknown number to be generated in
+userChoice = int(input("Guess the number from 1 to 100: "))  # prompts the user for the first guess
 
-unknownNumber = randint(1, 10) # sets the range of the unknown number to be generated in
+# define a function that will say if the guess is far or close
+def hot_cold(userChoice):
 
-userChoice = int(input("Guess the number from 1 to 10: ")) # prompts the user for the first guess
-
-while userChoice > 10 or userChoice < 0: # validates that the user inputs a number within the specified range
- print("Input outside range. Please try again.") # prints the appropriate message
- userChoice = int(input("Guess the number from 1 to 10: ")) # will keep prompting the user for a correct input if input is out of range
-
-tries = 2 # sets the tries counter to 2
-while userChoice != unknownNumber and tries != 0: # checks if the user's input matches the unknown number
-
-    if userChoice > 10 or userChoice < 0 : # validates that the user inputs a number within the specified range
-        print("Input outside range. Please try again.")  # prints the appropriate message
-        userChoice = int(input("Guess the number from 1 to 10: "))  # will keep prompting the user for a correct input if input is out of range
+    distance = abs(unknownNumber - userChoice)
+    if distance <= 10:
+        print("Hint!")
+        print("-" * 38)
+        print("the number is too close (range of 10)")
+        print("-" * 38)
+    elif distance <= 20:
+        print("Hint!")
+        print("-" * 33)
+        print("You are not so far (range of 20)")
+        print("-" * 33)
+    elif distance <= 30:
+        print("Hint!")
+        print("-" * 19)
+        print("Cold (range of 30)")
+        print("-" * 19)
     else:
-        tries = tries - 1  ## the tries counter will be changed accordingly
-        print(" Try Again. %d tries remaining " % tries) # if it doesnt match, this message will be printed
-        userChoice = int(input("Guess the number from 1 to 10: ")) ## and the user will be prompted again for an input
+        pass
+    print()
+tries = 4   # sets the tries counter to 4
+while userChoice != unknownNumber and tries != 0:    # checks if the user's input matches the unknown number
+
+    if userChoice > 100 or userChoice < 1:   # validates that the user inputs a number within the specified range
+        print("Input outside range. Please try again.")  # prints the appropriate message
+        userChoice = int(input("Guess the number from 1 to 100: "))      # will keep prompting the user for a correct input if input is out of range
+    else:
+        hot_cold(userChoice)
+        tries -= 1    # the tries counter will be changed accordingly
+        print(" Try Again. %d tries remaining " % (int(tries) + 1))    # if it doesnt match, this message will be printed
+        userChoice = int(input("Guess the number from 1 to 100: "))  # and the user will be prompted again for an input
 
 
-if userChoice == unknownNumber: # if the user's input matches the unknown number
-    print("yay you guessed correctly") ## the following message will be printed
+if userChoice == unknownNumber:     # if the user's input matches the unknown number
+    print("\nYah! you guessed correctly")  # the following message will be printed
 else:
-    print('You didnt guess the number in 3 tries. The number was %d' % unknownNumber) # if the user failed to guess, this message will be printed
+    print('''You didn't guess the number in 5 tries. The number was %d''' % unknownNumber)   # if the user failed to guess, this message will be printed
